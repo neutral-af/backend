9997a38e6bb6e4bcbf68e81891067556950cf55b
Jason Gwartz <jason.gwartz@ecosia.org>
2019-07-01 17:25:33 +0200

    starts http server

--- cmd/carbon-offsets/main.go
+++ cmd/carbon-offsets/main.go
@@ -1,20 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/jasongwartz/carbon-offset-backend/lib/cloverly"
 	"github.com/jasongwartz/carbon-offset-backend/lib/distance"
 	"github.com/jasongwartz/carbon-offset-backend/lib/emissions"
 )
 
+const port = "8080"
+
 func main() {
-	distance := distance.TwoAirports("YYZ", "LHR")
-	emissions := emissions.FlightCarbon(distance)
-	x, err := cloverly.Estimate(emissions)
-	if err != nil {
-		log.Panic(err)
-	}
-	fmt.Printf("%+v\n", x)
+	http.HandleFunc("/flight/estimate", func(w http.ResponseWriter, r *http.Request) {
+		params := r.URL.Query()
+
+		distance := distance.TwoAirports(params.Get("departure"), params.Get("arrival"))
+		emissions := emissions.FlightCarbon(distance)
+		estimate, err := cloverly.Estimate(emissions)
+		if err != nil {
+			log.Print(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		json.NewEncoder(w).Encode(estimate)
+	})
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
