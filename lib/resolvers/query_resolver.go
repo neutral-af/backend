3686b8e81dd8d61f6f9ee3d7ff93b203da37e36e
Jason Gwartz <jason@gwartz.me>
2019-11-24 00:12:15 +0100

    Log errors server-side

--- lib/resolvers/query_resolver.go
+++ lib/resolvers/query_resolver.go
@@ -2,26 +2,11 @@ package resolvers
 
 import (
 	"context"
-	"time"
 
-	"github.com/honeycombio/beeline-go"
-	"github.com/neutral-af/backend/lib/config"
 	models "github.com/neutral-af/backend/lib/graphql-models"
 )
 
 type queryResolver struct{ *Resolver }
-
-var startTime = time.Now()
-
-func (r *queryResolver) Health(ctx context.Context) (*models.Health, error) {
-	ctx, span := beeline.StartSpan(ctx, "health")
-	defer span.Send()
-
-	return &models.Health{
-		AliveSince:  int(startTime.Unix()),
-		Environment: config.C.Environment.ToString(),
-	}, nil
-}
 
 func (r *queryResolver) Estimate(ctx context.Context) (*models.GetEstimate, error) {
 	return &models.GetEstimate{}, nil
